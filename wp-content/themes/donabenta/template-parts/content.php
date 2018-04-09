905d8e891da1d989faea4830aa6ab9f046d87805
Drix <drix182@gmail.com>
2018-04-09 20:35:10 -0300

    menu responsivo

--- wp-content/themes/donabenta/template-parts/content.php
+++ wp-content/themes/donabenta/template-parts/content.php
@@ -16,10 +16,39 @@ $parentID = $category[0]->parent;
 $postid = get_the_ID();
 $post_title = get_the_title($postid);
 $category_link = get_category_link( $parentID );
+$thiscat = get_category( get_query_var( 'cat' ) );
+// $catid = $thiscat->cat_ID;
+// $parent = $thiscat->category_parent;
+
+$args = array( 'category' => $parentID, 'post_type' =>  'post', 'posts_per_page'   => -1 );
+$postslist = get_posts( $args );
+
 ?>
 <article id="interna" <?php post_class(); ?>>
 <div class="container">
-
+   <!-- Static navbar -->
+      <nav class="navbar navbar-default" id="menu-mobile">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Lista de Bares</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+             <?php   foreach ($postslist as $post) :  setup_postdata($post);
+                        $subpost_title = get_the_title($post->ID);
+              ?>
+                  <li <?php if($postid == $post->ID){echo 'class="active"';} ?>><a href="<?php if($postid == $post->ID){echo '#';}else{the_permalink();}  ?>"><?php echo strtolower($subpost_title); ?></a></li>
+              <?php endforeach; ?>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
   <div class="breadcrumb col-md-12 text-left">
       <p><a href="/" class="tt">HOME</a> / <a href="<?php echo esc_url( $category_link ); ?>" class="tt"><?php echo $parentcat; ?></a> / <a href="#"><?php echo $post_title?></a> </p>
   </div>
@@ -30,16 +59,6 @@ $category_link = get_category_link( $parentID );
   </div>
 
   <div class="sidebar col-md-2">
-  <?php
-
-    $thiscat = get_category( get_query_var( 'cat' ) );
-    // $catid = $thiscat->cat_ID;
-    // $parent = $thiscat->category_parent;
-
-    $args = array( 'category' => $parentID, 'post_type' =>  'post', 'posts_per_page'   => -1 );
-    $postslist = get_posts( $args );
-
-      ?>
 
       <div class="subcat-list">
           <ul>
@@ -109,19 +128,4 @@ $category_link = get_category_link( $parentID );
         </div>
     </div>
 </div>
-<div class="sidebar">
-      <div class="subcat-list mobile">
-          <ul>
-              <?php   foreach ($postslist as $post) :  setup_postdata($post);
-
-              $subpost_title = get_the_title($post->ID);
-
-               ?>
-
-                <li>
-                  <a href="<?php the_permalink(); ?>"><?php echo strtolower($subpost_title); ?></a></li>
-                <?php endforeach; ?>
-           </ul>
-      </div>
-      </div>
 </article><!-- #post-<?php the_ID(); ?> -->
